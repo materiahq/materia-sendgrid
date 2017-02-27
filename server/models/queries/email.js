b13acfa285864af707d89c537cfa34e3a2949f1a
Thibaud Arnault <thibaud.arnault@gmail.com>
2017-02-27 01:13:48 -0400

    configs in `sendgrid` key instead of `email`

--- server/models/queries/email.js
+++ server/models/queries/email.js
@@ -4,12 +4,12 @@ class EmailModel {
 	constructor(app, entity) {
 		this.app = app;
 		this.entity = entity;
-		this.emailSender = new SendgridSender(app.addons.addonsConfig['email'].apikey, app.addons.addonsConfig['email'].from)
+		this.emailSender = new SendgridSender(app.addons.addonsConfig['sendgrid'].apikey, app.addons.addonsConfig['sendgrid'].from)
 	}
 
 	send(params) {
 		return this.emailSender.send(params).then(() => {
-			params.from = this.app.addons.addonsConfig['email'].from
+			params.from = this.app.addons.addonsConfig['sendgrid'].from
 			params.date_sent = new Date()
 			return this.entity.getQuery('create')
 				.run(params)
