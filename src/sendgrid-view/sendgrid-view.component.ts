e6de2586daaa91be851b9babcce4938a1688e8e0
unknown <jeanmarc.royy@gmail.com>
2018-07-05 15:59:42 +0200

    (Migrations) Add sendDialog template + sendForm + Fix email-counter UI

--- src/sendgrid-view/sendgrid-view.component.ts
+++ src/sendgrid-view/sendgrid-view.component.ts
@@ -1,5 +1,16 @@
-import { Component, OnInit, Input, Output, EventEmitter, Inject } from "@angular/core";
+import {
+  Component,
+  OnInit,
+  Input,
+  Output,
+  EventEmitter,
+  Inject,
+  ViewChild,
+  TemplateRef
+} from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 import { HttpClient } from '@angular/common/http';
+import { MatDialog, MatSnackBar } from '@angular/material';
 import { Addon } from "@materia/addons";
 
 @Addon({
@@ -15,27 +26,58 @@ import { Addon } from "@materia/addons";
 })
 
 export class SendgridViewComponent implements OnInit {
+  emails: any;
+  nbEmails: number;
+  subjectControl: FormControl;
+  bodyControl: FormControl;
+  toControl: FormControl;
+  dialogRef: any;
+
   @Input() app;
   @Input() settings;
   @Input() baseUrl: string;
 
   @Output() openSetup = new EventEmitter<void>();
-  emails: any;
-  nbEmails: number;
 
-  constructor(@Inject('HttpClient') private http: HttpClient) { }
+  @ViewChild('sendDialog') sendDialog: TemplateRef<any>;
+
+  constructor(
+    @Inject('HttpClient') private http: HttpClient,
+    @Inject('MatDialog') private dialog: MatDialog,
+    @Inject('MatSnackBar') private snackBar: MatSnackBar
+) { }
 
   ngOnInit() {
-    this.init();
+    this.initForm();
+    this.listEmails();
   }
 
-  init() {
+  initForm() {
+    this.subjectControl = new FormControl('', Validators.required);
+    this.bodyControl = new FormControl('', Validators.required);
+    this.toControl = new FormControl('', Validators.required);
+  }
+
+  listEmails() {
     this.runQuery('sendgrid', 'latest').run().then(response => {
       this.emails = response.data
       this.nbEmails = response.count
     }).catch((err) => {
       console.log('error', err, err.stack)
     });
+  }
+
+  openSendDialog() {
+    this.initForm();
+    this.dialogRef = this.dialog.open(this.sendDialog);
+  }
+
+  send() {
+    this.runQuery('sendgrid', 'send', { subject: this.subjectControl.value, body: this.bodyControl.value, to: this.toControl.value }).then(() => {
+			this.dialogRef.close();
+			this.snackBar.open(`Email send`, null, { duration: 2000 });
+			this.listEmails();
+		});
   }
 
   private runQuery(entity: string, query: string, params?: any) {
